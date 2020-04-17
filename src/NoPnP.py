89566a19db6d983be2536a7a76c1ff447fe587e9
Christoph Henninger <chenninger@mail.de>
2020-04-17 18:41:08 +0200

    f2: Add CommandLineOption for config-file

--- src/NoPnP.py
+++ src/NoPnP.py
@@ -2,11 +2,21 @@ import configparser
 
 
 class NoPnP:
-    def __init__(self):
-        self.config = self.load_config()
+    def __init__(self, arguments=None):
+        configfile = 'nopnp.conf'
+        if arguments is not None:
+            status = 'n'
+            for arg in arguments:
+                if status == 'n':
+                    if arg == '-c':
+                        status = 'c'
+                elif status == 'c':
+                    configfile = arg
+
+        self.config = self.load_config(configfile)
 
     @staticmethod
-    def load_config():
+    def load_config(filename):
         config = configparser.ConfigParser()
-        config.read('nopnp.conf')
+        config.read(filename)
         return config
