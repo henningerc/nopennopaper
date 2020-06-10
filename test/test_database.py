749ea2ffcf0a529fef8faab241366708c9f054a1
Christoph Henninger <chenninger@mail.de>
2020-06-10 14:35:56 +0200

    f4: Create tests for empty database

--- test/test_database.py
+++ test/test_database.py
@@ -17,14 +17,14 @@ session: sqlalchemy.orm.Session
 fact: UUIDFactory
 
 
-@pytest.fixture(scope="class")
-def setup_database():
+# Prepare variables
+def prepare_variables():
     global to_delete
     global session
     global fact
-
     logging.basicConfig()
     logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
     config = {
         "server": "localhost",
         "database": "noPnP",
@@ -35,7 +35,24 @@ def setup_database():
     }
     Database(config)
     session = Database.Session()
+
     fact = UUIDFactory(config={"rootname": "nopnp.org"})
+
+
+def remove_to_delete():
+    global session
+    global to_delete
+
+    for row in to_delete:
+        session.delete(row)
+    session.commit()
+
+
+def setup_add_user():
+    global session
+    global fact
+    global to_delete
+
     v_user = User(id=str(fact.create_uuid("user", "Test")),
                   login="Test",
                   username="Test-Benutzer",
@@ -46,18 +63,32 @@ def setup_database():
     session.commit()
     to_delete.append(v_user)
 
+
+@pytest.fixture(scope="class")
+def fixture_database():
+    prepare_variables()
+    setup_add_user()
     yield
-    for row in to_delete:
-        session.delete(row)
-    session.commit()
-    pass
+    remove_to_delete()
+    return
 
 
-@mark.usefixtures("setup_database")
-class TestDatabase:
-    def test_connect(self):
-        assert Database.query_one_value("SELECT * FROM \"users\" WHERE login='Test'", "email") == "test@test.org"
+@pytest.fixture(scope="class")
+def fixture_empty_database():
+    prepare_variables()
+    yield
+    remove_to_delete()
+    return
 
+
+@pytest.fixture(scope='function')
+def fixture_add_user():
+    setup_add_user()
+    return
+
+
+@mark.usefixtures('fixture_empty_database')
+class TestEmptyDatabase:
     def test_user_save(self):
         global fact
         global session
@@ -70,11 +101,7 @@ class TestDatabase:
         assert_user = session.query(User).filter_by(login="Test2").first()
         assert assert_user.email == "zweiter.test@test.org"
 
-    def test_user_read(self):
-        global session
-        assert_user = session.query(User).filter_by(email="test@test.org").one()
-        assert assert_user.login == "Test"
-
+    @mark.usefixtures('fixture_add_user')
     def test_character_save(self):
         global to_delete
         global session
@@ -95,3 +122,14 @@ class TestDatabase:
             assert Database.query_one_value("SELECT * FROM \"characters\" WHERE name='Test Character'",
                                             "name") == "Test Character"
             session.commit()
+
+
+@mark.usefixtures("fixture_database")
+class TestDatabase:
+    def test_connect(self):
+        assert Database.query_one_value("SELECT * FROM \"users\" WHERE login='Test'", "email") == "test@test.org"
+
+    def test_user_read(self):
+        global session
+        assert_user = session.query(User).filter_by(email="test@test.org").one()
+        assert assert_user.login == "Test"
