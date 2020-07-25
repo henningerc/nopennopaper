c66ee2cef7115f431aa6b5f9791110817a38e48a
Christoph Henninger <chenninger@mail.de>
2020-07-25 10:08:04 +0200

    15.3: Change Buttons

--- src/views/static/js/management.js
+++ src/views/static/js/management.js
@@ -69,10 +69,10 @@
             row.off('click');
             row.children('td.title').html('<input type="text" class="title" att_id="' + d_in.id + '" value="' + d_in.title + '" />');
             row.children('td.description').html('<input type="text" class="description" att_id="' + d_in.id + '" value="' + d_in.description + '" />')
-            row.children('td.buttons').html('<button class="submit" att_id="' + d_in.id + '">speichern</button>'
-                    + '<button class="delete" att_id="' + d_in.id + '">löschen</button>');
-            $('button.submit[att_id="' + d_in.id + '"]').on('click', submitAttributeForm);
-            $('button.delete[att_id="' + d_in.id + '"]').on('click', clickDeleteAttribute);
+            row.children('td.buttons').html('<a href="javascript:void(0)" class="submit" att_id="' + d_in.id + '">(&check;)</a>'
+                    + '<a href="javascript:void(0)" class="delete" att_id="' + d_in.id + '">(X)</a>');
+            $('a.submit[att_id="' + d_in.id + '"]').on('click', submitAttributeForm);
+            $('a.delete[att_id="' + d_in.id + '"]').on('click', clickDeleteAttribute);
         });
     }
 
@@ -91,10 +91,10 @@
             $('tr#' + data.id + ' td.standard')
                 .html('<input type="checkbox" class="standard" head_id="' + data.id + '"' + (data.standard?' checked':'') + '/>');
             $('tr#' + data.id + ' td.buttons')
-                .html('<button class="submit" head_id="' + data.id + '">speichern</button>'
-                    + '<button class="delete" head_id="' + data.id + '">löschen</button>');
-            $('button.submit[head_id="' + data.id + '"]').on('click', submitHeadForm);
-            $('button.delete[head_id="' + data.id + '"]').on('click', clickDeleteHead);
+                .html('<a href="javascript:void(0)" class="submit" head_id="' + data.id + '">(&check;)</a>'
+                    + '<a href="javascript:void(0)" class="delete" head_id="' + data.id + '">(X)</a');
+            $('a.submit[head_id="' + data.id + '"]').on('click', submitHeadForm);
+            $('a.delete[head_id="' + data.id + '"]').on('click', clickDeleteHead);
         });
     }
 
@@ -105,10 +105,10 @@
             + '<td class="description"><input type="text" class="description" head_id="new_header" value="" /></td>'
             + '<td class="order"><input type="number" class="order" head_id="new_header" value="" /></td>'
             + '<td class="standard"><input type="checkbox" class="standard" head_id="new_header" /></td>'
-            + '<td class="buttons"><button class="submit" head_id="new_header">speichern</button></td>'
+            + '<td class="buttons"><a href="javascript:void(0)" class="submit" head_id="new_header">(&check;)</a></td>'
             + '</tr>';
         $('table#headers').append(new_tr);
-        $('button.submit[head_id="new_header"]').on('click', submitHeadForm);
+        $('a.submit[head_id="new_header"]').on('click', submitHeadForm);
     });
 
     $('a#new_attribute').on('click', function(event){
@@ -116,10 +116,10 @@
         var new_tr = '<tr id="new_attribute" class="editable attribute">'
             + '<td class="title"><input type="text" class="title" att_id="new_attribute" value="" /></td>'
             + '<td class="description"><input type="text" class="description" att_id="new_attribute" value="" /></td>'
-            + '<td class="buttons"><button class="submit" att_id="new_attribute">speichern</button></td>'
+            + '<td class="buttons"><a href="javascript:void(0)" class="submit" att_id="new_attribute">(&check;)</a></td>'
             + '</tr>';
         $('table#attributes').append(new_tr);
-        $('button.submit[att_id="new_attribute"]').on('click', submitAttributeForm);
+        $('a.submit[att_id="new_attribute"]').on('click', submitAttributeForm);
     });
 
     $('tr.head_value').on('click', showHeadForm);
